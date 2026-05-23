1f32a5ac30c79088c8fa736aea85d1c3fa09461c
Irene587 <sara.aalaoui5807@gmail.com>
2026-05-23 20:41:43 +0200

    Add files via upload

---
+++ graphic.h
@@ -0,0 +1,11 @@
+#ifndef GRAPHICS_H
+#define GRAPHICS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+void draw_monster(int monster_id, bool is_boss);
+void draw_class(int class_type);
+
+#endif
